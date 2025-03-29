0ad5fe53a98ba261d4dea943ab4927ec2577edad
YanaNaumova <naumovayana87@gmail.com>
2025-03-29 21:15:06 +0100

    clear input

--- src/components/feedbackForm/index.jsx
+++ src/components/feedbackForm/index.jsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
+
 function FeedbackForm() {
-  let inputName = "";
+  const [inputName, setInputName] = useState("");
 
   const handelInputChange = (event) => {
-    inputName = event.target.value;
+    setInputName(event.target.value);
     console.log(inputName);
   };
 
   const handelSubmit = (event) => {
     event.preventDefault();
+    setInputName("");
     console.log(inputName);
   };
   return (
     <form onSubmit={handelSubmit}>
       <label>Введите имя</label>
-      <input type="text" onChange={handelInputChange} />
+      <input type="text" onChange={handelInputChange} value={inputName} />
       <input type="submit" />
     </form>
   );
